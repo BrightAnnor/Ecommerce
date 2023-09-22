943c30f4ed7c6d7a72b9dc1b80fc74adb2684dcb
BrightAnnor <annor170bright@gmail.com>
2023-09-22 19:27:32 +0000

    update: integration the login-admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,9 +1,13 @@
 const index = (req,res)=>{
     res.render('pages/admin/pages/index')
 };
+const ViewLogin = (req,res)=>{
+    res.render('pages/admin/pages/login',{layout:'pages/admin/layouts/other'})
+};
 
 
 
 module.exports = {
-    index
+    index,
+    ViewLogin
 }
